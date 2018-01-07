b2d80f113e3e04c30b22352bf53d54f54274b33e
Daniel Gee <dan@dan-gee.co.uk>
2018-01-07 22:18:44 +0000

    amend module tests to check exported functions

--- Tests/DigitalOceanApi.Tests.ps1
+++ Tests/DigitalOceanApi.Tests.ps1
@@ -10,7 +10,7 @@ Describe 'DigitalOceanApi Module' {
         $expectedCommands | ForEach-Object {
         Context $_ {
             It 'Exists in module'{
-                $_ | Should -BeIn (Get-Module DigitalOceanApi).ExportedCommands.Keys
+                $_ | Should -BeIn (Get-Module DigitalOceanApi).ExportedFunctions.Keys
             }
         }
     }
