c870371b13e25fb72ce2fe89015827d30b4d1a53
Daniel Gee <dan@dan-gee.co.uk>
2018-01-14 17:12:43 +0000

    exclude some of the scaffolding files from code coverage

--- ExecutePester.ps1
+++ ExecutePester.ps1
@@ -12,6 +12,6 @@ $scriptFolder = "$BuildSourcesDirectory"
 $CodeCoverageOutputFile = "$BuildSourcesDirectory\CodeCoverage-Pester.XML"
 $resultsFile = "$BuildSourcesDirectory\Test-Pester.XML"
 
- $Files = Get-ChildItem -Path $scriptFolder -include *.ps1, *.psm1 -Exclude *.Tests.ps1 -Recurse | Select-object -ExpandProperty Fullname
+ $Files = Get-ChildItem -Path $scriptFolder -include *.ps1, *.psm1 -Exclude *.Tests.ps1, ExecutePester.ps1, ModuleLoader.ps1 -Recurse | Select-object -ExpandProperty Fullname
 
 Invoke-Pester -OutputFile $resultsFile -OutputFormat 'NUnitXml' -Script $scriptFolder -CodeCoverage $Files -CodeCoverageOutputFile $CodeCoverageOutputFile -EnableExit
