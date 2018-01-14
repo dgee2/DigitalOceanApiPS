8f55c8c0a04e9e6ce7ed774dcc86f97277cad6aa
Daniel Gee <dan@dan-gee.co.uk>
2018-01-14 17:01:44 +0000

    amend pester script to exit and give return code to vsts

--- ExecutePester.ps1
+++ ExecutePester.ps1
@@ -12,19 +12,6 @@ $scriptFolder = "$BuildSourcesDirectory"
 $CodeCoverageOutputFile = "$BuildSourcesDirectory\CodeCoverage-Pester.XML"
 $resultsFile = "$BuildSourcesDirectory\Test-Pester.XML"
 
-$Parameters = @{
-    OutputFile = $resultsFile
-    OutputFormat = 'NUnitXml'
-    Script = $scriptFolder
-}
-
  $Files = Get-ChildItem -Path $scriptFolder -include *.ps1, *.psm1 -Exclude *.Tests.ps1 -Recurse | Select-object -ExpandProperty Fullname
 
-if ($Files) {
-    $Parameters.Add('CodeCoverage', $Files)
-    $Parameters.Add('CodeCoverageOutputFile', $CodeCoverageOutputFile)
-} else {
-    Write-Warning -Message "No PowerShell files found under $scriptFolder to analyse for code coverage."
-}
-
-Invoke-Pester @Parameters
+Invoke-Pester -OutputFile $resultsFile -OutputFormat 'NUnitXml' -Script $scriptFolder -PassThru -EnableExit -CodeCoverage $Files -CodeCoverageOutputFile $CodeCoverageOutputFile
