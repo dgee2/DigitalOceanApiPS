ab6a3a416239f7b6c7f0a4c823598961e244f9fa
Daniel Gee <dan@dan-gee.co.uk>
2018-01-28 16:01:40 +0000

    use InModuleScope to scope tests

--- Tests/Get-DigitalOceanAction.Tests.ps1
+++ Tests/Get-DigitalOceanAction.Tests.ps1
@@ -1,139 +1,140 @@
-. $PSScriptRoot/../Module/ModuleLoader.ps1
+Import-Module $PSScriptRoot/../Module/DigitalOceanApi.psd1
 
-$id = 36804636
-$testAction = @{
-    id = $id
-    status = 'completed'
-    type = 'create'
-    started_at = '2014-11-14T16:29:21Z'
-    completed_at = '2014-11-14T16:30:06Z'
-    resource_id = 3164444
-    resource_type = 'droplet'
-    region = 'nyc3'
-    region_slug = 'nyc3'
-}
-$testResponse = @{
- 	action = @(
+InModuleScope DigitalOceanApi {
+    $id = 36804636
+    $testAction = @{
+        id = $id
+        status = 'completed'
+        type = 'create'
+        started_at = '2014-11-14T16:29:21Z'
+        completed_at = '2014-11-14T16:30:06Z'
+        resource_id = 3164444
+        resource_type = 'droplet'
+        region = 'nyc3'
+        region_slug = 'nyc3'
+    }
+    $testResponse = @{
+        action = @(
+            $testAction
+        )
+    }
+    $testListResponse = @{
+        actions = @(
         $testAction
-    )
- }
- $testListResponse = @{
-    actions = @(
-       $testAction
-    )
-    links = @{
-       pages = @{
-           last = 'https://api.digitalocean.com/v2/actions?page=159&per_page=1'
-           next = 'https://api.digitalocean.com/v2/actions?page=2&per_page=1'
-       }
+        )
+        links = @{
+        pages = @{
+            last = 'https://api.digitalocean.com/v2/actions?page=159&per_page=1'
+            next = 'https://api.digitalocean.com/v2/actions?page=2&per_page=1'
+        }
+        }
+        meta = @{
+            total = 159
+        }
     }
-    meta = @{
-        total = 159
-    }
-}
-$testToken = "$((new-guid).guid -replace @('-',''))$((new-guid).guid -replace @('-',''))"
+    $testToken = "$((new-guid).guid -replace @('-',''))$((new-guid).guid -replace @('-',''))"
+    Describe 'Get-DigitalOceanAction' {
+        Context 'ID Parameter Set' {
+            Mock Invoke-DigitalOceanApiCall -MockWith {$testResponse}
+            Context 'Request'{
+                Get-DigitalOceanAction -Token $testToken -Id $id
+                It 'Passes the correct token'{
+                    Assert-MockCalled Invoke-DigitalOceanApiCall -Exactly 1 -ParameterFilter { $Token -eq $testToken }
+                }
+                It 'Sends to the correct url'{
+                    Assert-MockCalled Invoke-DigitalOceanApiCall -Exactly 1 -ParameterFilter { $Url -eq "actions/$id" }
+                }
+            }
+            Context 'Response' {
+                $response = Get-DigitalOceanAction -Token $testToken -Id $id
 
-Describe 'Get-DigitalOceanAction' {
-    Context 'ID Parameter Set' {
-        Mock Invoke-DigitalOceanApiCall -MockWith {$testResponse}
-        Context 'Request'{
-            Get-DigitalOceanAction -Token $testToken -Id $id
-            It 'Passes the correct token'{
-                Assert-MockCalled Invoke-DigitalOceanApiCall -Exactly 1 -ParameterFilter { $Token -eq $testToken }
-            }
-            It 'Sends to the correct url'{
-                Assert-MockCalled Invoke-DigitalOceanApiCall -Exactly 1 -ParameterFilter { $Url -eq "actions/$id" }
-            }
-        }
-        Context 'Response' {
-            $response = Get-DigitalOceanAction -Token $testToken -Id $id
-
-            It 'Parses Id correctly'{
-                $response.Id | Should -Be $testAction.id
-            }
-            It 'Parses Status correctly'{
-                $response.Status | Should -Be $testAction.status
-            }
-            It 'Parses Type correctly'{
-                $response.Type | Should -Be $testAction.type
-            }
-            It 'Parses StartedAt correctly'{
-                $response.StartedAt | Should -Be ([datetime]$testAction.started_at)
-            }
-            It 'Parses CompletedAt correctly'{
-                $response.CompletedAt | Should -Be ([datetime]$testAction.completed_at)
-            }
-            It 'Parses ResourceId correctly'{
-                $response.ResourceId | Should -Be $testAction.resource_id
-            }
-            It 'Parses ResourceType correctly'{
-                $response.ResourceType | Should -Be $testAction.resource_type
-            }
-            It 'Parses Region correctly'{
-                $response.Region | Should -Be $testAction.region_slug
-            }
-        }
-    }
-    Context 'Default Parameter Set' {
-        Mock Invoke-DigitalOceanApiCall -MockWith {$testListResponse}
-        Context 'Request'{
-            Get-DigitalOceanAction -Token $testToken
-            It 'Passes the correct token'{
-                Assert-MockCalled Invoke-DigitalOceanApiCall -Exactly 1 -ParameterFilter { $Token -eq $testToken }
-            }
-            It 'Sends to the correct url'{
-                Assert-MockCalled Invoke-DigitalOceanApiCall -Exactly 1 -ParameterFilter { $Url -eq 'actions' }
-            }
-            Context 'Paging'{
-                It 'Calls correct url for page'{
-                    Get-DigitalOceanAction -Token $testToken -Page 15
-                    Assert-MockCalled Invoke-DigitalOceanApiCall -Exactly 1 -ParameterFilter { $Url -eq 'actions?page=15' }
-                }
-                It 'Calls correct url for per page'{
-                    Get-DigitalOceanAction -Token $testToken -PerPage 23
-                    Assert-MockCalled Invoke-DigitalOceanApiCall -Exactly 1 -ParameterFilter { $Url -eq 'actions?per_page=23' }
-                }
-                It 'Calls correct url for per page and page'{
-                    Get-DigitalOceanAction -Token $testToken -Page 15 -PerPage 23
-                    Assert-MockCalled Invoke-DigitalOceanApiCall -Exactly 1 -ParameterFilter { $Url -eq 'actions?page=15&per_page=23' }
-                }
-            }
-        }
-        Context 'Response' {
-           $response = Get-DigitalOceanAction -Token $testToken
-   
-            Context 'Actions'{
-                It 'Contains the correct number'{
-                    ($response.Actions | Measure-Object).Count | Should -Be 1
-                }
                 It 'Parses Id correctly'{
-                    $response.Actions[0].Id | Should -Be $testAction.id
+                    $response.Id | Should -Be $testAction.id
                 }
                 It 'Parses Status correctly'{
-                    $response.Actions[0].Status | Should -Be $testAction.status
+                    $response.Status | Should -Be $testAction.status
                 }
                 It 'Parses Type correctly'{
-                    $response.Actions[0].Type | Should -Be $testAction.type
+                    $response.Type | Should -Be $testAction.type
                 }
                 It 'Parses StartedAt correctly'{
-                    $response.Actions[0].StartedAt | Should -Be ([datetime]$testAction.started_at)
+                    $response.StartedAt | Should -Be ([datetime]$testAction.started_at)
                 }
                 It 'Parses CompletedAt correctly'{
-                    $response.Actions[0].CompletedAt | Should -Be ([datetime]$testAction.completed_at)
+                    $response.CompletedAt | Should -Be ([datetime]$testAction.completed_at)
                 }
                 It 'Parses ResourceId correctly'{
-                    $response.Actions[0].ResourceId | Should -Be $testAction.resource_id
+                    $response.ResourceId | Should -Be $testAction.resource_id
                 }
                 It 'Parses ResourceType correctly'{
-                    $response.Actions[0].ResourceType | Should -Be $testAction.resource_type
+                    $response.ResourceType | Should -Be $testAction.resource_type
                 }
                 It 'Parses Region correctly'{
-                    $response.Actions[0].Region | Should -Be $testAction.region_slug
+                    $response.Region | Should -Be $testAction.region_slug
                 }
             }
-            Context 'TotalCount'{
-                It 'Parses TotalCount correctly'{
-                    $response.TotalCount | Should -Be $testListResponse.meta.total
+        }
+        Context 'Default Parameter Set' {
+            Mock Invoke-DigitalOceanApiCall -MockWith {$testListResponse}
+            Context 'Request'{
+                Get-DigitalOceanAction -Token $testToken
+                It 'Passes the correct token'{
+                    Assert-MockCalled Invoke-DigitalOceanApiCall -Exactly 1 -ParameterFilter { $Token -eq $testToken }
+                }
+                It 'Sends to the correct url'{
+                    Assert-MockCalled Invoke-DigitalOceanApiCall -Exactly 1 -ParameterFilter { $Url -eq 'actions' }
+                }
+                Context 'Paging'{
+                    It 'Calls correct url for page'{
+                        Get-DigitalOceanAction -Token $testToken -Page 15
+                        Assert-MockCalled Invoke-DigitalOceanApiCall -Exactly 1 -ParameterFilter { $Url -eq 'actions?page=15' }
+                    }
+                    It 'Calls correct url for per page'{
+                        Get-DigitalOceanAction -Token $testToken -PerPage 23
+                        Assert-MockCalled Invoke-DigitalOceanApiCall -Exactly 1 -ParameterFilter { $Url -eq 'actions?per_page=23' }
+                    }
+                    It 'Calls correct url for per page and page'{
+                        Get-DigitalOceanAction -Token $testToken -Page 15 -PerPage 23
+                        Assert-MockCalled Invoke-DigitalOceanApiCall -Exactly 1 -ParameterFilter { $Url -eq 'actions?page=15&per_page=23' }
+                    }
+                }
+            }
+            Context 'Response' {
+            $response = Get-DigitalOceanAction -Token $testToken
+    
+                Context 'Actions'{
+                    It 'Contains the correct number'{
+                        ($response.Actions | Measure-Object).Count | Should -Be 1
+                    }
+                    It 'Parses Id correctly'{
+                        $response.Actions[0].Id | Should -Be $testAction.id
+                    }
+                    It 'Parses Status correctly'{
+                        $response.Actions[0].Status | Should -Be $testAction.status
+                    }
+                    It 'Parses Type correctly'{
+                        $response.Actions[0].Type | Should -Be $testAction.type
+                    }
+                    It 'Parses StartedAt correctly'{
+                        $response.Actions[0].StartedAt | Should -Be ([datetime]$testAction.started_at)
+                    }
+                    It 'Parses CompletedAt correctly'{
+                        $response.Actions[0].CompletedAt | Should -Be ([datetime]$testAction.completed_at)
+                    }
+                    It 'Parses ResourceId correctly'{
+                        $response.Actions[0].ResourceId | Should -Be $testAction.resource_id
+                    }
+                    It 'Parses ResourceType correctly'{
+                        $response.Actions[0].ResourceType | Should -Be $testAction.resource_type
+                    }
+                    It 'Parses Region correctly'{
+                        $response.Actions[0].Region | Should -Be $testAction.region_slug
+                    }
+                }
+                Context 'TotalCount'{
+                    It 'Parses TotalCount correctly'{
+                        $response.TotalCount | Should -Be $testListResponse.meta.total
+                    }
                 }
             }
         }
